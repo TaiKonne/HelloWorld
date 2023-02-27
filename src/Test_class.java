2959849246d63ce54d0d34b612db670957909dae
TaiKonne <taichuotchuoi@gmail.com>
2023-02-27 16:43:23 +0700

    tesst

--- src/Test_class.java
+++ src/Test_class.java
@@ -1,7 +1,13 @@
 import java.util.*;
 
 public class Test_class {
+    static class solution{
+        public void maxx()
+        {}
+    }
     public void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        new solution().
+        sc.close();
     }
 }
