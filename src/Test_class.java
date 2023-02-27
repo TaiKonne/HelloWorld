aba274d085d682c2f54b84b675acfedbd4810c62
quocdung2603 <2124802010008@student.tdmu.edu.vn>
2023-02-27 16:44:06 +0700

    checked 4:44 pm

--- src/Test_class.java
+++ src/Test_class.java
@@ -1,7 +1,16 @@
 import java.util.*;
 
 public class Test_class {
-    public void main(String[] args) {
+    static class Solution
+    {
+        void hello(Scanner sc)
+        {
+            System.out.println("KKKKKKKKKKKKK");
+        }
+    }
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        new Solution().hello(sc);
+        sc.close();
     }
 }
